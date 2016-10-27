adecbd0533c57747583e364fbcdeb16c8a7e41c4
jonathan thom and suzi rubino <jonathan@>
2016-10-27 11:46:15 -0700

    Handle final digit of numer

--- roman-numerals/js/scripts.js
+++ roman-numerals/js/scripts.js
@@ -7,7 +7,7 @@ function properRange(userInput) {
 
 function converter(userInput) {
 
-  var output = [];
+  var outputArray = [];
 
   if (userInput === "5") {
     console.log("if statement is running");
@@ -23,7 +23,41 @@ function converter(userInput) {
   } else if (userInput === "1000") {
     return "M";
   }
-}
+
+    var inputArray = userInput.split("");
+
+    var finalNumber = inputArray.pop();
+    var secondToLastNumber = inputArray.pop();
+    var thirdToLastNumber = inputArray.pop();
+    var fourthToLastNumber = inputArray.pop();
+
+
+    if (finalNumber === "1") {
+        outputArray.push("I");
+    } else if (finalNumber === "2") {
+        outputArray.push("II");
+    } else if (finalNumber === "3") {
+      outputArray.push("III");
+    } else if (finalNumber === "4") {
+      outputArray.push("IV");
+    } else if (finalNumber === "5") {
+      outputArray.push("V");
+    } else if (finalNumber === "6") {
+      outputArray.push("VI");
+    } else if (finalNumber === "7") {
+      outputArray.push("VII");
+    } else if (finalNumber === "8") {
+      outputArray.push("VIII");
+    } else if (finalNumber === "9") {
+      outputArray.push("XI");
+    } else if (finalNumber === "0") {
+      outputArray.push("X");
+      // will need to add exceptions to this rule for divisible by 10, 50, etc...
+    }
+
+  var finalOutput = outputArray.join("");
+  return finalOutput;
+} //converter
 
 
 
@@ -37,7 +71,6 @@ $(document).ready(function(){
     var userInput = $("#userNumber").val();
     var inRange = properRange(userInput);
     var result = converter(userInput);
-    console.log(result);
     if (inRange === false) {
       $(".output").text("Please enter a number between 1 and 3,999");
     } else {
